56e161da62b58487154050b0334f7cfc036353d4
jhahn678 <jhahn678@yahoo.com>
2022-10-25 00:34:21 -0400

    testing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', routes)
+app.use('/test', routes)
 app.get('/', (req, res) => res.status(200).json({ message: 'this works' }))
 app.listen(process.env.PORT || 3050, () => {
     console.log('app listening')
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.get('/test', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({ test: 'testing this endpoint'})
 })
 

e373a0848361c1477007e1baf570d297ed45dd14
jhahn678 <jhahn678@yahoo.com>
2022-10-24 21:58:00 -0400

    .

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
+import { Request, Response } from "express"
+import express from 'express'
 require('dotenv').config()
-const express = require('express')
 
 const app = express()
-app.get('/', (req, res) => res.status(200).json({ message: 'this works' }))
+app.get('/', (req: Request, res: Response) => res.status(200).json({ message: 'this works' }))
 app.listen(process.env.PORT || 3050, () => {
     console.log('app listening')
 })

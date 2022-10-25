b05c10149cf37039a9b2ce28e266d381a0acd0cb
jhahn678 <jhahn678@yahoo.com>
2022-10-25 10:55:33 -0400

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import express from 'express'
 import path from 'path'
-import { logName } from './utils/logName'
+import routes from './routes'
 require('dotenv').config()
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/test', (req, res) => {
-    logName('Terrence')
-    res.status(200).json({ test: 'testing this endpoint'})
-})
+app.use('/test', routes)
 app.get('/', (req, res) => res.status(200).json({ message: 'this works' }))
 app.listen(process.env.PORT || 3050, () => {
     console.log(__dirname)

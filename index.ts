185187aad1af78634c4114b4fb5571421abb024a
jhahn678 <jhahn678@yahoo.com>
2022-10-24 22:34:49 -0400

    testing public files

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import path from 'path'
 require('dotenv').config()
 
 const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.status(200).json({ message: 'this works' }))
 app.listen(process.env.PORT || 3050, () => {
     console.log('app listening')

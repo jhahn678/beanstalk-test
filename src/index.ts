cbb085d832922eb7cdb74dc61215c6da2c590582
jhahn678 <jhahn678@yahoo.com>
2022-10-25 01:31:57 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/test', (req, res) => {
+app.get('/test', (req, res) => {
     logName('Terrence')
     res.status(200).json({ test: 'testing this endpoint'})
 })
